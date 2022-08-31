e707845d9e4c4641e7320206cb643467ac51f419
Len Groves <lgroves317@gmail.com>
2022-08-31 11:27:20 -0400

    Initial Commit

---
+++ H4xor News/H4xor_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4xor_NewsApp.swift
+//  H4xor News
+//
+//  Created by Leonard Groves on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct H4xor_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
